434d32ef5197d8d25d79cceb1b3046bdd667dae5
Vutik <vutik.vasek@gmail.com>
2025-08-29 14:48:04 +0200

    search history dependent on id

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useAuth } from './AuthContext';
 
 export const AppContext = createContext();
 
@@ -6,6 +7,8 @@ export const AppProvider = ({children}) => {
   const [toastText, setToastText] = useState('');
   const [toastColor, setToastColor] = useState('');
   const [toastReshow, setToastReshow] = useState(false);
+
+  const { id } = useAuth();
 
   let showInfoToast = (text) => showToast(text, "yellow");
   let showErrorToast = (text) => showToast(text, "red");
@@ -18,25 +21,41 @@ export const AppProvider = ({children}) => {
   const [searchHistory, setSearchHistory] = useState(JSON.parse(localStorage.getItem('searches')));
 
   const addToSearchHistory = (obj) => {
-    const array = JSON.parse(localStorage.getItem('searches')) || [];
-    array.unshift(obj);
-    const newHistory = array.filter((val, index, arr) => arr.findIndex(obj => JSON.stringify(obj) === JSON.stringify(val)) === index).slice(0, 30);
-    localStorage.setItem('searches', JSON.stringify(newHistory));
+    const idPairs = JSON.parse(localStorage.getItem('searches')) || [];
+    if (!idPairs.some(obj => obj.id === id)) idPairs.push({ id, history: [] });
+    const history = idPairs.find(obj => obj.id === id)?.history || [];
+    history.unshift(obj);
+    const newHistory = history.filter((val, index, arr) => arr.findIndex(obj => JSON.stringify(obj) === JSON.stringify(val)) === index).slice(0, 30);
+    idPairs.find(obj => obj.id === id).history = newHistory;
+    localStorage.setItem('searches', JSON.stringify(idPairs));
     setSearchHistory(newHistory);
   }
 
   const deleteFromSearchHistory = (index) => {
-    const array = JSON.parse(localStorage.getItem('searches'));
-    if (!array || array.length === 0) return;
-    array.splice(index, 1);
-    localStorage.setItem('searches', JSON.stringify(array));
-    setSearchHistory(array);
+    const idPairs = JSON.parse(localStorage.getItem('searches'));
+    const history = idPairs?.find(obj => obj.id === id)?.history;
+    if (!idPairs || !history) return;
+    history.splice(index, 1);
+    idPairs.find(obj => obj.id === id).history = history;
+    localStorage.setItem('searches', JSON.stringify(idPairs));
+    setSearchHistory(history);
   }
 
-  const clearSearchHistory = () =>{
-    localStorage.setItem('searches', JSON.stringify([]));
+  const clearSearchHistory = () => {
+    const idPairs = JSON.parse(localStorage.getItem('searches'));
+    const history = idPairs?.find(obj => obj.id === id)?.history;
+    if (!idPairs || !history) return;
+    idPairs.find(obj => obj.id === id).history = [];
+    localStorage.setItem('searches', JSON.stringify(idPairs));
     setSearchHistory([]);
   }
+
+  useEffect(() => {
+    if (!id) return setSearchHistory([]);
+    const idPairs = JSON.parse(localStorage.getItem('searches'));
+    const history = idPairs?.find(obj => obj.id === id)?.history || [];
+    setSearchHistory(history);
+  }, [id])
 
   return (
     <AppContext.Provider value={{
